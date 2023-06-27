1f94e12fd0d1c9a79e5b1019138dae1deb1bdf59
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-06-27 10:35:45 +0530

    set a bit in a program

---
+++ set bit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Bits{
+
+     public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n= sc.nextInt();
+        System.out.println("Enter the position");
+        int pos= sc.nextInt();
+        int bitMask=1<<pos;
+        
+        int newNumber = bitMask | n;
+        System.out.println(newNumber);
+      
+     }
+}
